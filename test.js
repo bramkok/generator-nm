22d44bb61a2362171ce9435eaf1f1d4b03e02da1
Bram Kok <bram@bramkok.com>
2016-08-15 16:31:51 +0200

    Change code style of all files

--- test.js
+++ test.js
@@ -7,133 +7,136 @@ import moduleName from './app/module-name';
 
 let generator;
 
-test.beforeEach(async () => {
-	await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
-	generator = helpers.createGenerator('node-modules:app', ['../app'], null, {skipInstall: true});
+test.beforeEach(async() => {
+  await pify(helpers.testDirectory)(path.join(__dirname, 'temp'));
+  generator = helpers.createGenerator('node-modules:app', ['../app'], null, {
+    skipInstall: true,
+  });
 });
 
-test.serial('generates expected files', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false
-	});
+test.serial('generates expected files', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: false,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.file([
-		'.editorconfig',
-		'.eslintrc.json',
-		'.git',
-		'.gitattributes',
-		'.gitignore',
-		'.travis.yml',
-		'index.js',
-		'license',
-		'package.json',
-		'readme.md',
-		'test.js'
-	]);
+  assert.file([
+    '.editorconfig',
+    '.eslintrc.json',
+    '.git',
+    '.gitattributes',
+    '.gitignore',
+    '.travis.yml',
+    'index.js',
+    'license',
+    'package.json',
+    'readme.md',
+    'test.js',
+  ]);
 
-	assert.noFile('cli.js');
+  assert.noFile('cli.js');
 });
 
-test.serial('CLI option', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: true
-	});
+test.serial('CLI option', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: true,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.file('cli.js');
-	assert.fileContent('package.json', /"bin":/);
-	assert.fileContent('package.json', /"bin": "cli.js"/);
-	assert.fileContent('package.json', /"meow"/);
+  assert.file('cli.js');
+  assert.fileContent('package.json', /"bin":/);
+  assert.fileContent('package.json', /"bin": "cli.js"/);
+  assert.fileContent('package.json', /"meow"/);
 });
 
-test.serial('nyc option', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false,
-		nyc: true,
-		coveralls: false
-	});
+test.serial('nyc option', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: false,
+    nyc: true,
+    coveralls: false,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.noFile('cli.js');
-	assert.fileContent('.gitignore', /\.nyc_output/);
-	assert.fileContent('.gitignore', /coverage/);
-	assert.fileContent('package.json', /"xo && nyc ava"/);
-	assert.fileContent('package.json', /"nyc": "/);
-	assert.noFileContent('package.json', /"coveralls":/);
-	assert.noFileContent('package.json', /"lcov"/);
-	assert.noFileContent('.travis.yml', /coveralls/);
+  assert.noFile('cli.js');
+  assert.fileContent('.gitignore', /\.nyc_output/);
+  assert.fileContent('.gitignore', /coverage/);
+  assert.fileContent('package.json', /"xo && nyc ava"/);
+  assert.fileContent('package.json', /"nyc": "/);
+  assert.noFileContent('package.json', /"coveralls":/);
+  assert.noFileContent('package.json', /"lcov"/);
+  assert.noFileContent('.travis.yml', /coveralls/);
 });
 
-test.serial('coveralls option', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false,
-		nyc: true,
-		coveralls: true
-	});
+test.serial('coveralls option', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: false,
+    nyc: true,
+    coveralls: true,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.noFile('cli.js');
-	assert.fileContent('.gitignore', /\.nyc_output/);
-	assert.fileContent('.gitignore', /coverage/);
-	assert.fileContent('package.json', /"xo && nyc ava"/);
-	assert.fileContent('package.json', /"nyc": "/);
-	assert.fileContent('package.json', /"coveralls":/);
-	assert.fileContent('package.json', /"lcov"/);
-	assert.fileContent('.travis.yml', /coveralls/);
+  assert.noFile('cli.js');
+  assert.fileContent('.gitignore', /\.nyc_output/);
+  assert.fileContent('.gitignore', /coverage/);
+  assert.fileContent('package.json', /"xo && nyc ava"/);
+  assert.fileContent('package.json', /"nyc": "/);
+  assert.fileContent('package.json', /"coveralls":/);
+  assert.fileContent('package.json', /"lcov"/);
+  assert.fileContent('.travis.yml', /coveralls/);
 });
 
-test('parse scoped package names', t => {
-	t.is(moduleName.slugify('author/thing'), 'author-thing', 'slugify non-scoped packages');
-	t.is(moduleName.slugify('@author/thing'), '@author/thing', 'accept scoped packages');
-	t.is(moduleName.slugify('@author/hi/there'), 'author-hi-there', 'fall back to regular slugify if invalid scoped name');
+test('parse scoped package names', (t) => {
+  t.is(moduleName.slugify('author/thing'), 'author-thing', 'slugify non-scoped packages');
+  t.is(moduleName.slugify('@author/thing'), '@author/thing', 'accept scoped packages');
+  t.is(moduleName.slugify('@author/hi/there'),
+    'author-hi-there', 'fall back to regular slugify if invalid scoped name');
 });
 
-test.serial('prompts for description', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		moduleDescription: 'foo',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false,
-		nyc: true,
-		coveralls: true
-	});
+test.serial('prompts for description', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    moduleDescription: 'foo',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: false,
+    nyc: true,
+    coveralls: true,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.fileContent('package.json', /"description": "foo",/);
-	assert.fileContent('readme.md', /> foo/);
+  assert.fileContent('package.json', /"description": "foo",/);
+  assert.fileContent('readme.md', /> foo/);
 });
 
-test.serial('defaults to superb description', async () => {
-	helpers.mockPrompt(generator, {
-		moduleName: 'test',
-		githubUsername: 'test',
-		website: 'test.com',
-		cli: false,
-		nyc: true,
-		coveralls: true
-	});
+test.serial('defaults to superb description', async() => {
+  helpers.mockPrompt(generator, {
+    moduleName: 'test',
+    githubUsername: 'test',
+    website: 'test.com',
+    cli: false,
+    nyc: true,
+    coveralls: true,
+  });
 
-	await pify(generator.run.bind(generator))();
+  await pify(generator.run.bind(generator))();
 
-	assert.fileContent('package.json', /"description": "My .+ module",/);
-	assert.fileContent('readme.md', /> My .+ module/);
+  assert.fileContent('package.json', /"description": "My .+ module",/);
+  assert.fileContent('readme.md', /> My .+ module/);
 });
